3a85c2d249056ee8b60063462502a7e6f5cb389d
tha-dev-miit <theinhtetaung.dev@gmail.com>
2026-04-10 14:34:43 +0630

    add test case for stack push

--- TDD/StackTDD/Test/test_stack.c
+++ TDD/StackTDD/Test/test_stack.c
@@ -1,17 +1,29 @@
-#include<stdio.h>
+#include <stdio.h>
 #include <assert.h>
 #include "C:\Users\DELL\Desktop\DSA\TDD\StackTDD\Src\stack.h"
+#include "C:\Users\DELL\Desktop\DSA\TDD\StackTDD\Src\stack.c"
+
+Stack s;
 
 void test_init()
 {
-    Stack s;
-    init(&s);
-    assert(s.top == -1);
-    printf("test_init passed\n");
+        init(&s);
+        assert(s.top == -1);
+        printf("test_init passed\n");
+}
+
+void test_push()
+{
+        init(&s);
+        push(&s, 10);
+        assert(s.top == 0);
+        assert(s.items[s.top] == 10);
+        printf("test_push passed\n");
 }
 
 int main()
 {
-    test_init();
-    return 0;
+        test_init();
+        test_push();
+        return 0;
 }
