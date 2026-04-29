5fa71b7ee583369302b916a87e5b915c53956bfa
tha-dev-miit <theinhtetaung.dev@gmail.com>
2026-04-29 22:28:26 +0630

    test: add initial test for queue creation (Red)

---
+++ TDD/QueueTDD/Test/test_queue.c
@@ -0,0 +1,14 @@
+#include <assert.h>
+#include <stdio.h>
+#include "queue.h"
+
+void test_create_queue() {
+    Queue* q = create_queue(5);
+    assert(is_empty(q) == 1); 
+    printf("Test Create Queue: Passed\n");
+}
+
+int main() {
+    test_create_queue();
+    return 0;
+}
